920ca5fe8e1f2927c09c7a53871a3a2403dfe8df
Megan Lee <melee124@gmail.com>
2018-03-24 10:30:00 -0700

    hid result div, added some js for the pizza toppings.

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,11 @@ $(document).ready(function() {
     event.preventDefault();
     $("#pizza-order").hide();
     $("#userOrder").show();
-    //$("input:checkbox[name=work-transport]:checked").each(function() {
-      //var workTransport = $(this).val();
-      //$(".work-commute").append(workTransport + "<br>");
-    //  this.checked=false;
-    //});
+    $("input:checkbox[name=toppings]:checked").each(function() {
+      var pizzaToppings = $(this).val();
+      $(".pizzaMe").append(pizzaToppings + "<br>");
+      this.checked=false;
+    });
     //$("input:checkbox[name=fun-transport]:checked").each(function() {
       //var funTransport = $(this).val();
     //  $(".fun-commute").append(funTransport + "<br>");
