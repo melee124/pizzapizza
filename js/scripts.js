2ced3f8d07271f35a33c929a4cddc51b52ac1c64
Megan Lee <melee124@gmail.com>
2018-04-02 14:05:54 -0700

    fixed js so that at least the result is displayed, although pizzaSize is not working, so the result is NaN.

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,49 @@
 //Business Logic
 function Pizza(size, toppings, price) {
+  debugger;
   this.pizzaSize = size;
   this.pizzaToppings = toppings;
   this.pizzaPrice = price;
 };
 
 Pizza.prototype.totalPrice = function() {
+  return (this.pizzaToppings.length + this.sizePrice).toFixed();
+};
+
+var sizePrice = function(size) {
+  debugger;
   var price = 0;
-  if (pizzaSize === 1) {
+  if (size === 1) {
     price = 12;
-  } else if (pizzaSize === 2) {
+  } else if (size === 2) {
     price = 15;
-  } else if (pizzaSize === 3) {
+  } else if (size === 3) {
     price = 18;
-  } else if (pizzaSize === 4) {
+  } else if (size === 4) {
     price = 24;
   }
     return price;
   };
 
-  //  if (toppingsArray.length < 1){
-    //  total += 0;
-    //  this.price = total;
-  //  } else {
-    // for (var i = 0; i < toppingsArray.length; i++) {
-    //    total += 1;
-      //  this.price = total;
-    //  }
-  //  }
-//  }
-//Pizza.prototype.cost = function( size, toppings, price) {
-//  return "$" + this.price + " " + this.size + "pizza, with" + this.toppings + ".";
-//};
 //UI Logic
 $(document).ready(function() {
-  $("form#pizza-order").submit(function(event) {
+  $("#pizza-order").submit(function(event) {
     event.preventDefault();
     debugger;
     var pizzaToppings = [];
     var size = $('#pizzaSize').val();
     var price = sizePrice(size);
-    $("input:checkbox[name=topping]:checked").each(function))s {
-      var toppings = $(this).val();
-      toppings.push(topping);
-    });
+    var toppings = $("input:checkbox[name=topping]:checked");
+      for(var i=0; i<toppings.length; i++) {
+        pizzaToppings.push($(toppings[i]).val());
+      }
 
-    var newPizza = new Pizza(size, toppings, price);
+    var newPizza = new Pizza(size, pizzaToppings, price);
     var userTotal = newPizza.totalPrice();
 
     $('#userOrder').show();
     $('.order').text(newPizza.pizzaSize + "" + newPizza.toppings + "pizza.");
+    $('#total').text("$" + userTotal);
     $('#pizza-order').hide();
     });
-
-    this.checked=false;
     });
